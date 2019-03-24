11a0f840be291c4b7658b2c8b4dbfbd7937d0014
JasaNetSoft <jasa.net15@gmail.com>
2019-03-24 19:16:58 -0400

    crud-articulo

---
+++ src/app/_shared/fakebackend.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, of, throwError , EMPTY} from 'rxjs';
+import { delay, mergeMap, materialize, dematerialize } from 'rxjs/operators';
+ 
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+
+    private fullName: String    = 'John Doe';
+    private email: String    = 'john@doe.com';
+
+    constructor() { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return of(null).pipe(mergeMap(() => {
+
+            const auth    = request.headers.get('Authorization');
+
+            if (request.url.endsWith('/api/items') && request.method === 'GET') {
+
+                console.log('[ intercepting ] ' + request.method + ' : ' + request.url + ' ' + auth);
+
+                const body = {
+                    result : [
+                        {
+                            title : 'Articulo 1',
+                            description : 'Descripción 1',
+                            id : 1
+                        },
+                        {
+                            title : 'Articulo 2',
+                            description : 'Descripción 2',
+                            id : 2
+                        }
+                    ]
+                };
+                return of(new HttpResponse({ status: 200, body: body }));
+            }
+
+            // Add item
+            if (request.url.endsWith('/api/item') && request.method === 'POST') {
+
+                console.log('[ intercepting ] ' + request.method + ' : ' + request.url + ' ' + auth);
+
+                const bodyPosted    = request.body;
+                const random    = (Math.floor(Math.random() * Math.floor(1000)) + 1);
+                const newResult    = { ...bodyPosted , id: random};
+
+                const body = {
+                    success : bodyPosted ? true : false,
+                    result : newResult
+                };
+                return of(new HttpResponse({ status: 200, body: body }));
+            }
+
+            // Update item
+            if (request.url.startsWith('/api/item') && request.method === 'PUT') {
+
+                console.log('[ intercepting ] ' + request.method + ' : ' + request.url + ' ' + auth);
+
+                const bodyPosted    = request.body;
+
+                const body = {
+                    success : bodyPosted ? true : false,
+                    result : bodyPosted
+                };
+                return of(new HttpResponse({ status: 200, body: body }));
+            }
+
+            // Delete item
+            if (request.url.startsWith('/api/item/') && request.method === 'DELETE') {
+                console.log('[ intercepting ] ' + request.method + ' : ' + request.url + ' ' + auth);
+                const body = {
+                    success : true
+                };
+                return of(new HttpResponse({ status: 200, body: body }));
+            }
+
+            return next.handle(request);
+        }))
+        .pipe(materialize())
+        .pipe(delay(500))
+        .pipe(dematerialize());
+    }
+
+    private makeid(): string {
+        let  text = '';
+        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (let i = 0; i < 25; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    }
+}
